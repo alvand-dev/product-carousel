275bf3733ed487cd61aa9ff5cfd7bc79a1c18427
Mohammad177 <m17768@gmail.com>
2021-11-17 14:20:39 +0330

    Fix featured-products-slider when reaching last slide and change the screen size problem.

--- assets/js/featured-products-slider.js
+++ assets/js/featured-products-slider.js
@@ -11,6 +11,8 @@ const sliderPrev = document.querySelector('#featured-product .slider-arrow-prev'
 // Define variables
 let itemIndex = 0;
 let numberOfItems;
+
+// Define media queries
 const mediaQueryList = [
   window.matchMedia('(max-width: 575px)'),
   window.matchMedia('(max-width: 991px)'),
@@ -20,7 +22,8 @@ const mediaQueryList = [
 // Display slider items
 const displayItems = () => {
   let html = '';
-  for(let i = itemIndex; i < itemIndex + numberOfItems; i++) {
+  let len = itemIndex + numberOfItems;
+  for(let i = itemIndex; i < len; i++) {
     html += sliderItems[i].outerHTML;
   }
   sliderList.innerHTML = html;
@@ -28,10 +31,42 @@ const displayItems = () => {
 
 // Handel number of images base on device size
 const HandleScreen = () => {
-  if(mediaQueryList[0].matches) {numberOfItems = 1;}
-  else if(mediaQueryList[1].matches) {numberOfItems = 2;}
-  else if(mediaQueryList[2].matches) {numberOfItems = 3;}
-  else {numberOfItems = 4;}
+  if(mediaQueryList[0].matches) {
+    numberOfItems = 1;
+    itemIndex = 0;
+    // set width and height to item for when image does not load
+    sliderItems.forEach((item) => {
+      item.style.width = '100%';
+      item.style.minHeight = '150px';
+    });
+  }
+  else if(mediaQueryList[1].matches) {
+    numberOfItems = 2;
+    itemIndex = 0;
+    // set width and height to item for when image does not load
+    sliderItems.forEach((item) => {
+      item.style.width = '50%';
+      item.style.minHeight = '150px';
+    });
+  }
+  else if(mediaQueryList[2].matches) {
+    numberOfItems = 3;
+    itemIndex = 0;
+    // set width and height to item for when image does not load
+    sliderItems.forEach((item) => {
+      item.style.width = '35%';
+      item.style.minHeight = '150px';
+    });
+  }
+  else {
+    numberOfItems = 4;
+    itemIndex = 0;
+    // set width and height to item for when image does not load
+    sliderItems.forEach((item) => {
+      item.style.width = '25%';
+      item.style.minHeight = '150px';
+    });
+  }
   displayItems();
 };
 
