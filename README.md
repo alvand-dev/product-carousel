87ac977ad1c220f266ffb46d282ca3112efcd723
alvand1399 <alvand1399@gmail.com>
2022-04-06 15:54:22 +0430

    Change: The slider method of showing slides Fix: The flashing images bug when clicking on the next and prev buttons Fix: Page jump to top bug when clicking on next and previous buttons Add: Animation when changing slides to the left or right Improve: Showing sliders better on mobile device horizontal mode Improve: Make the code more neat and clean

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Responsive Product Carousel (Slider)
 
-This is a responsive product carousel (slider) built from scratch without any framework or library (just vanilla HTML, CSS, and JavaScript). It has few lines of code so is lightweight and fast. I used the for loop method for this with a class that you can create as many sliders as you want on a single webpage from it. All used images are free to use and from <a href="https://unsplash.com/" target="_blank">Unsplash</a>.
+This is a responsive product carousel (slider) built from scratch without any framework or library (just vanilla HTML, CSS, and JavaScript). It has few lines of code so is lightweight and fast. I used a class that you can create as many sliders as you want on a single webpage from it. All used images are free to use and from [Unsplash](https://unsplash.com/).
--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,82 +1,93 @@
 'use strict';
 
 {
-  // A template class for sliders
+  // A class for building sliders from it
   class Slider {
-    constructor(id, mediaQuery) {
+    constructor(id, mediaQueries) {
       // Get HTML elements
       this.slider = document.querySelector(`#${id}`);
       this.sliderList = this.slider.querySelector('.slider-list');
       this.sliderItems = this.slider.querySelectorAll('.slider-item');
       this.sliderNext = this.slider.querySelector('.slider-arrow-next');
       this.sliderPrev = this.slider.querySelector('.slider-arrow-prev');
+
+      // Get media queries
       this.mediaQueryList = [
-        window.matchMedia(`screen and (max-width:${mediaQuery[0] - 1}px)`),
+        window.matchMedia(`screen and (max-width:${mediaQueries[0] - 1}px)`),
       ];
-      mediaQuery.forEach((item) => {
+      mediaQueries.forEach((mediaQuery) => {
         this.mediaQueryList.push(
-          window.matchMedia(`screen and (min-width:${item}px)`)
+          window.matchMedia(`screen and (min-width:${mediaQuery}px)`)
         );
       });
-      // Define global variables
-      this.numberOfItems;
-      this.itemIndex;
-      this.flag = true;
-    }
 
-    // Handel number of slides and set slides width + event listeners
-    run() {
-      this.mediaQueryList.forEach((mediaQuery, index) => {
-        if (mediaQuery.matches) {
-          this.numberOfItems = index + 1;
-          this.itemIndex = 0;
-          this.sliderItems.forEach((item) => {
-            item.style.width = `${100 / this.numberOfItems}%`;
-          });
+      // Define global variables
+      this.numberOfVisibleItems;
+      this.currentItemIndex;
+      this.sliderItemsLength = this.sliderItems.length;
+      this.mediaQueryLength = this.mediaQueryList.length;
+
+      // Add event listener: to call the run function again when screen resized
+      this.mediaQueryList.forEach((mediaQuery) => {
+        mediaQuery.addEventListener('change', () => {
+          this.run();
+        });
+      });
+
+      // Add event listener: to go to next slide
+      this.sliderNext.addEventListener('click', () => {
+        if (
+          this.currentItemIndex <
+          this.sliderItemsLength - this.numberOfVisibleItems
+        ) {
+          this.currentItemIndex++;
+          this.shiftSlides();
         }
       });
 
-      // Run displayItems function
-      this.displayItems();
+      // Add event listener: to go to previous slide
+      this.sliderPrev.addEventListener('click', () => {
+        if (this.currentItemIndex > 0) {
+          this.currentItemIndex--;
+          this.shiftSlides();
+        }
+      });
+    }
 
-      // Event listeners
-      if (this.flag === true) {
-        // Add listener to media-queries to call the run function again when screen resized
-        this.mediaQueryList.forEach((mediaQuery) => {
-          mediaQuery.addEventListener('change', () => {
-            this.run();
+    // Run the slider
+    run() {
+      let index = this.mediaQueryLength - 1;
+      while (index >= 0) {
+        if (this.mediaQueryList[index].matches) {
+          // Set number of visible slides
+          this.numberOfVisibleItems = index + 1;
+
+          // Reset the slider
+          this.currentItemIndex = 0;
+          this.sliderList.style.transform = 'translateX(0)';
+
+          // Set slider list width
+          this.sliderList.style.width = `${
+            (100 / this.numberOfVisibleItems) * this.sliderItemsLength
+          }%`;
+
+          // Set slides width
+          this.sliderItems.forEach((item) => {
+            item.style.width = `${100 / this.numberOfVisibleItems}%`;
           });
-        });
 
-        // Display next slide
-        this.sliderNext.addEventListener('click', () => {
-          if (this.itemIndex < this.sliderItems.length - this.numberOfItems) {
-            this.itemIndex++;
-            this.displayItems();
-          }
-        });
-
-        // Display Previous slide
-        this.sliderPrev.addEventListener('click', () => {
-          if (this.itemIndex > 0) {
-            this.itemIndex--;
-            this.displayItems();
-          }
-        });
-
-        // Prevent adding event listener each time run function called
-        this.flag = false;
+          // Exit the loop
+          break;
+        }
+        index--;
       }
     }
 
-    // Display slider items
-    displayItems() {
-      let html = '';
-      let len = this.itemIndex + this.numberOfItems;
-      for (let i = this.itemIndex; i < len; i++) {
-        html += this.sliderItems[i].outerHTML;
-      }
-      this.sliderList.innerHTML = html;
+    // A function to shift slides left and right
+    shiftSlides() {
+      this.sliderList.style.transform = `translateX(-${
+        (100 / this.sliderItemsLength) * this.currentItemIndex
+      }%`;
     }
   }
 
